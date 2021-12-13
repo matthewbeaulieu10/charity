9ec283b38e71aa5cca842b0c6e24b33b130f564d
matthew <matthewbeaulieu10@gmail.com>
2021-12-13 01:53:42 -0600

    charity work

---
+++ austin.js
@@ -0,0 +1,7 @@
+function YourMother(){
+    document.querySelector('#changeable').innerHTML = '<p>your mom</p>';
+}
+
+function YourFather(){
+    document.querySelector('#changeable').innerHTML = '<p>your dad</p>';
+}
